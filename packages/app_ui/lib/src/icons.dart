6e78eafe925463e403cd4afc5dc466356faf036f
Ethan <ethanokamura3@gmail.com>
2024-12-27 01:27:04 -0800

    added calendar icon

--- packages/app_ui/lib/src/icons.dart
+++ packages/app_ui/lib/src/icons.dart
@@ -60,29 +60,19 @@ class AppIcons {
   static const IconData settings = FontAwesomeIcons.gear;
   static const IconData share = FontAwesomeIcons.share;
   static const IconData cancel = FontAwesomeIcons.xmark;
-  static const IconData message = FontAwesomeIcons.paperPlane;
   static const IconData more = FontAwesomeIcons.ellipsis;
-  static const IconData comment = FontAwesomeIcons.comment;
-  static const IconData camera = FontAwesomeIcons.camera;
   static const IconData delete = FontAwesomeIcons.trash;
   static const IconData edit = FontAwesomeIcons.pencil;
-  static const IconData block = FontAwesomeIcons.userLock;
   static const IconData next = FontAwesomeIcons.arrowRight;
   static const IconData back = FontAwesomeIcons.arrowLeft;
 
   // toggles
-  static const IconData saved = FontAwesomeIcons.solidBookmark;
-  static const IconData notSaved = FontAwesomeIcons.bookmark;
-  static const IconData liked = FontAwesomeIcons.solidHeart;
-  static const IconData notLiked = FontAwesomeIcons.heart;
   static const IconData checked = FontAwesomeIcons.solidSquareCheck;
   static const IconData notChecked = FontAwesomeIcons.square;
   static const IconData lightMode = FontAwesomeIcons.sun;
   static const IconData darkMode = FontAwesomeIcons.moon;
 
   // misc
-  static const IconData activity = FontAwesomeIcons.mountain;
-  static const IconData posts = FontAwesomeIcons.images;
   static const IconData boards = FontAwesomeIcons.list;
 
   // pages
@@ -91,4 +81,5 @@ class AppIcons {
   static const IconData create = FontAwesomeIcons.plus;
   static const IconData inbox = FontAwesomeIcons.inbox;
   static const IconData user = FontAwesomeIcons.solidUser;
+  static const IconData month = FontAwesomeIcons.solidCalendarDays;
 }

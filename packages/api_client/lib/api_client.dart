09fd9ff37b846e73b00a282c495fbe1f1cd486b2
Ethan <ethanokamura3@gmail.com>
2024-12-26 21:56:13 -0800

    created a library to handle api's used within the app

---
+++ packages/api_client/lib/api_client.dart
@@ -0,0 +1,7 @@
+/// App UI
+library;
+
+export 'src/api_client.dart';
+export 'package:isar/isar.dart';
+export 'package:path_provider/path_provider.dart';
+export 'package:isar_flutter_libs/isar_flutter_libs.dart';

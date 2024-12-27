dd057e81843fcfb35357bf05542eafe40045c35d
Ethan <ethanokamura3@gmail.com>
2024-12-27 01:29:31 -0800

    implemented generic months, so now the user can move backwards and forwards to see progress over the year

--- lib/features/heatmap/view/hm_month.dart
+++ lib/features/heatmap/view/hm_month.dart
@@ -1,18 +1,20 @@
 import 'package:app_ui/app_ui.dart';
 
 class MonthlyHeatMap extends StatelessWidget {
-  const MonthlyHeatMap({required this.datasets, super.key});
+  const MonthlyHeatMap({
+    required this.datasets,
+    required this.month,
+    required this.year,
+    super.key,
+  });
   final Map<DateTime, int> datasets;
+  final int month;
+  final int year;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final currentDate = DateTime(
-      DateTime.now().year,
-      DateTime.now().month,
-      DateTime.now().day,
-    );
-    final days = getDaysInMonth(currentDate.year, currentDate.month);
+    final days = getDaysInMonth(year, month);
     return GridView.builder(
       itemCount: days,
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -22,8 +24,7 @@ class MonthlyHeatMap extends StatelessWidget {
       ),
       physics: NeverScrollableScrollPhysics(),
       itemBuilder: (context, index) {
-        final date = DateTime(currentDate.year, currentDate.month, 1)
-            .add(Duration(days: index));
+        final date = DateTime(year, month, 1).add(Duration(days: index));
         final int value = datasets.containsKey(date) ? datasets[date]! : 0;
         return Container(
           alignment: Alignment.center,
@@ -43,16 +44,4 @@ class MonthlyHeatMap extends StatelessWidget {
       },
     );
   }
-}
-
-int getDaysInMonth(int year, int month) {
-  // Create a DateTime object for the first day of the next month
-  final nextMonth = month == 12 ? 1 : month + 1;
-  final nextMonthDate = DateTime(year, nextMonth, 1);
-
-  // Subtract one day from the first day of the next month to get the last day of the current month
-  final lastDayOfMonth = nextMonthDate.subtract(const Duration(days: 1));
-
-  // Return the day of the month, which represents the number of days in the month
-  return lastDayOfMonth.day;
 }

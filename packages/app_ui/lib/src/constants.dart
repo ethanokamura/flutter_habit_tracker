f37098dac9ea260befb3a8a92ca1003adff80120
Ethan <ethanokamura3@gmail.com>
2024-12-27 21:04:27 -0800

    corrected path to images

--- packages/app_ui/lib/src/constants.dart
+++ packages/app_ui/lib/src/constants.dart
@@ -6,5 +6,5 @@ const double defaultSpacing = 10;
 const double defaultRadius = 5;
 const BorderRadius defaultBorderRadius =
     BorderRadius.all(Radius.circular(defaultRadius));
-const String defaultDarkImage = 'assets/images/dark_mode_face.png';
-const String defaultLightImage = 'assets/images/light_mode_face.png';
+const String defaultDarkImage = 'assets/dark_mode_face.png';
+const String defaultLightImage = 'assets/light_mode_face.png';

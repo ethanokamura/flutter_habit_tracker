3b288184fb866e9721942fa196bc39ec9375b707
Ethan <ethanokamura3@gmail.com>
2024-12-27 21:07:38 -0800

    updated impprt

--- lib/features/profile/view/profile_page.dart
+++ lib/features/profile/view/profile_page.dart
@@ -1,7 +1,7 @@
 import 'package:app_core/app_core.dart';
 import 'package:app_ui/app_ui.dart';
 import 'package:habit_tracker/features/profile/cubit/profile_cubit.dart';
-import 'package:habit_tracker/features/profile/profile/view/profile_view.dart';
+import 'package:habit_tracker/features/profile/view/profile_view.dart';
 import 'package:habit_tracker/l10n/l10n.dart';
 import 'package:user_repository/user_repository.dart';
 

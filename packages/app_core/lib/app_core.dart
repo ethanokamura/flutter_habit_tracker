649304eb2ca19d1f0a4bf7ee25b24dcb3e322a3a
Ethan <ethanokamura3@gmail.com>
2024-12-27 19:06:57 -0800

    added supabase to the project

--- packages/app_core/lib/app_core.dart
+++ packages/app_core/lib/app_core.dart
@@ -7,6 +7,7 @@ export 'package:bloc/bloc.dart';
 export 'package:equatable/equatable.dart';
 export 'package:flow_builder/flow_builder.dart';
 export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:flutter_dotenv/flutter_dotenv.dart';
 export 'package:flutter_localizations/flutter_localizations.dart';
 export 'package:shared_preferences/shared_preferences.dart';
 export 'package:json_annotation/json_annotation.dart';

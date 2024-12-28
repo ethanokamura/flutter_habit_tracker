5c1d2968c63d612cbb43f7538d0c6d6f7195579e
Ethan <ethanokamura3@gmail.com>
2024-12-27 19:12:34 -0800

    initialized supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:api_client/api_client.dart';
+import 'package:app_core/app_core.dart';
 import 'package:habit_repository/habit_repository.dart';
 import 'package:habit_tracker/app/app.dart';
+import 'package:user_repository/user_repository.dart';
 
 Future<void> main() async {
   try {
@@ -8,29 +10,36 @@ Future<void> main() async {
       init: () async {
         try {
           // Perform any necessary setup here
+          await dotenv.load();
+          await Supabase.initialize(
+            url: dotenv.env['SUPABASE_URL']!,
+            anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+            realtimeClientOptions: const RealtimeClientOptions(
+              logLevel: RealtimeLogLevel.error,
+            ),
+          );
         } catch (e) {
           throw Exception('Database initialization error: $e');
         }
       },
       builder: () async {
-        try {
-          // Initialize database
-          final dir = await getApplicationDocumentsDirectory();
-          final isar = await Isar.open(
-            [HabitSchema, AppSettingsSchema],
-            directory: dir.path,
-          );
-          // Initialize production dependencies
-          final habitRepository = HabitRepository(isar);
-          await habitRepository.loadInitialHabits();
-          await habitRepository.saveFirstLaunchDate();
+        final userRepository = UserRepository();
+        await userRepository.getOpeningUser();
+        // Initialize database
+        final dir = await getApplicationDocumentsDirectory();
+        final isar = await Isar.open(
+          [HabitSchema, AppSettingsSchema],
+          directory: dir.path,
+        );
+        // Initialize production dependencies
+        final habitRepository = HabitRepository(isar);
+        await habitRepository.loadInitialHabits();
+        await habitRepository.saveFirstLaunchDate();
 
-          return App(
-            habitRepository: habitRepository,
-          );
-        } catch (e) {
-          throw Exception('Bootstrap error: $e');
-        }
+        return App(
+          userRepository: userRepository,
+          habitRepository: habitRepository,
+        );
       },
     );
   } catch (e) {

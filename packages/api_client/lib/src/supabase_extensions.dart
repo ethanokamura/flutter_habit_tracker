a19970d9c9ede32646cc06a60dbf1566fa18b678
Ethan <ethanokamura3@gmail.com>
2025-01-13 19:37:48 -0800

    removed image methods

--- packages/api_client/lib/src/supabase_extensions.dart
+++ packages/api_client/lib/src/supabase_extensions.dart
@@ -1,4 +1,3 @@
-import 'dart:typed_data';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 extension SupabaseExtensions on SupabaseClient {
@@ -8,47 +7,4 @@ extension SupabaseExtensions on SupabaseClient {
   SupabaseQueryBuilder fromUsersTable() => from('users');
   SupabaseQueryBuilder fromHabitsTable() => from('habits');
   SupabaseQueryBuilder fromHabitCompletionsTable() => from('habit_completions');
-
-  /// Upload A File
-  /// [collection] the collection the file is stored in
-  /// [file] the file that needs to be uploaded
-  Future<String> uploadFile({
-    required String collection,
-    required String path,
-    required Uint8List file,
-  }) async {
-    try {
-      await Supabase.instance.client.storage.from(collection).uploadBinary(
-            path,
-            file,
-          );
-      return Supabase.instance.client.storage
-          .from(collection)
-          .getPublicUrl(path);
-    } catch (e) {
-      throw Exception('Error uploading file $e');
-    }
-  }
-
-  /// Update An Existing File
-  /// [collection] the collection the file is stored in
-  /// [file] the file that needs to be uploaded
-  Future<String> updateFile({
-    required String collection,
-    required String path,
-    required Uint8List file,
-  }) async {
-    try {
-      await Supabase.instance.client.storage.from(collection).updateBinary(
-            path,
-            file,
-            fileOptions: const FileOptions(upsert: true),
-          );
-      return Supabase.instance.client.storage
-          .from(collection)
-          .getPublicUrl(path);
-    } catch (e) {
-      throw Exception('Error uploading file $e');
-    }
-  }
 }
